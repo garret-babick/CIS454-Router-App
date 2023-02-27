75bb01a5a8cacb3cbb5c8114121f3e2758369a50
Jiashu Li <71674444+jli130@users.noreply.github.com>
2023-02-27 13:03:05 -0500

    Add files via upload

---
+++ Router/app/src/main/java/com/example/router/Routes.java
@@ -0,0 +1,15 @@
+package com.example.router;
+
+public class Routes {
+    String routesType;
+    String routesStart;
+    String routesEnd;
+    String routesTime;
+
+    public Routes(String routesType, String routesStart, String routesEnd, String routesTime) {
+        this.routesType = routesType;
+        this.routesStart = routesStart;
+        this.routesEnd = routesEnd;
+        this.routesTime = routesTime;
+    }
+}
